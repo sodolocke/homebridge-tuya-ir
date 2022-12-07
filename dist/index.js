1e5142faf5691ec7d3a51874025a76ecbdf7c24e
Paween Dherapattana <sodolocke@gmail.com>
2022-12-07 07:19:10 +0000

    version name update

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 const platform_1 = require("./platform");
-const PLATFORM_NAME = 'TuyaIR';
+const PLATFORM_NAME = 'TuyaIR2';
 module.exports = (api) => {
     api.registerPlatform(PLATFORM_NAME, platform_1.TuyaIRPlatform);
 };
